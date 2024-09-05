273e513f036960dc4acb29bf393e718e1a54e2f5
codeWithAmitKumar <work.amitpatra@gmail.com>
2024-09-05 19:45:38 +0530

    just setup my project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+        <div className="container w-screen min-h-screen overflow-x-hidden bg-[#0E0E0E] text-white">
+          <div className="middle w-[100vw] min-h-[70%] bg-red-500 ">amit</div>
+        </div>
+    </>
+  )
+}
+
+export default App
